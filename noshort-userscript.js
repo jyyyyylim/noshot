f619c814637123cb703cf8ca7e8da40d221ba7d4
俊。 <junyaolim0206@gmail.com>
2022-12-29 13:24:53 +0800

    they get the point now

--- noshort-userscript.js
+++ noshort-userscript.js
@@ -4,6 +4,7 @@
 // @version      0.1
 // @description  shorts ui is soy trash
 // @author       j5ylim
+// @icon         https://github.com/jyyyyylim/noshot/raw/main/nosh.png
 // @grant        none
 // @include      *://www.youtube.com/shorts/*
 // @run-at       document-start
