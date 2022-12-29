c6cc8608863a6a1f4a2f6a977430e8c3984f2336
俊。 <junyaolim0206@gmail.com>
2022-12-29 14:18:09 +0800

    Create noshort.user.js

---
+++ noshort.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         noshorts
+// @namespace    https://www.youtube.com/
+// @version      0.1
+// @description  shorts ui is soy trash
+// @author       j5ylim
+// @icon         https://github.com/jyyyyylim/noshot/raw/main/nosh.png
+// @grant        none
+// @include      *://www.youtube.com/shorts/*
+// @run-at       document-start
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    let url = new URL(window.location.href);
+
+    url.searchParams.append("v", url.pathname.substring(url.pathname.lastIndexOf('/')+1));
+    url.pathname = "watch"; window.location.replace(url);
+})();
